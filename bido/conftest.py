02df9329b0957c67b4b0e635e51d8e7bb6f384cf
Johnny Ordoñez <johnny.ord.mzk@gmail.com>
2020-12-20 18:40:55 -0500

    First awesone commit for Bido

---
+++ bido/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bido.users.models import User
+from bido.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
